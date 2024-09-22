5302a31c8f3c4f9c7c6c94bc7bb3d2dd85e97073
Oldranda1414 <leonardo.randacio@gmail.com>
2024-09-22 18:15:45 +0200

    fix: updated version of sonarqube to try and fix CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     id("io.github.cosmicsilence.scalafix") version "0.2.2"
 
     // Add sonarqube plugin for CI pipeline
-    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.0.0.2929"
     // id("jacoco")
 }
 
