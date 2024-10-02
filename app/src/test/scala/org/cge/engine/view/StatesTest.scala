cf9d78fbc9188321f6fe0fcca1cb9787e462f299
Oldranda1414 <leonardo.randacio@gmail.com>
2024-10-02 18:50:34 +0200

    refactoring: added test for States file

---
+++ app/src/test/scala/org/cge/engine/view/StatesTest.scala
@@ -0,0 +1,31 @@
+package org.cge.engine.view
+
+import org.cge.AnyTest
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.BeforeAndAfterEach
+
+class StateTest extends AnyTest with BeforeAndAfterEach:
+  import States._
+  import Monads.*
+
+  private var state: State[Int, String] = State(s => (s + 1, s"state: $s"))
+
+  override def beforeEach(): Unit =
+    state = State(s => (s + 1, s"state: $s"))
+
+  test("State should transform the state and return the expected value"):
+    val (newState, result) = state(0)
+    newState should be (1)
+    result should be ("state: 0")
+
+  test("StateMonad unit should create a state that returns the given value"):
+    val unitState = summon[Monad[[A] =>> State[Int, A]]].unit("test")
+    val (newState, result) = unitState(0)
+    newState should be (0)
+    result should be ("test")
+
+  test("StateMonad flatMap should chain state transformations"):
+    val chainedState = state.flatMap(s => State(s2 => (s2 + 2, s"chained: $s2")))
+    val (newState, result) = chainedState(0)
+    newState should be (3)
+    result should be ("chained: 1")
