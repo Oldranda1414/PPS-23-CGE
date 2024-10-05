277cb37d2c6f905291137c63009ed8d68a4a3105
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-10-05 16:55:36 +0200

    Refactor: devided builder logic from dsl logic

--- app/src/main/scala/org/cge/engine/GameBuilder.scala
+++ app/src/main/scala/org/cge/engine/GameBuilder.scala
@@ -1,7 +1,8 @@
-package org.cge.dsl
+package org.cge.engine
 
-import org.cge.engine.Game
 import org.cge.engine.Game.SimpleGame
+import org.cge.engine.data.StandardDeck
+import org.cge.engine.Game.SimplePlayer
 
 /** A trait that defines a GameBuilder. */
 trait GameBuilder:
@@ -26,6 +27,14 @@ trait GameBuilder:
   def addPlayer(name: String): GameBuilder
 
   /**
+    * Sets the number of cards in hand for each player.
+    *
+    * @param numberOfCards the number of cards in hand
+    * @return the GameBuilder instance
+    */
+  def cardsInHand(numberOfCards: Int): GameBuilder
+
+  /**
    * Builds the game.
    *
    * @return the game
@@ -33,17 +42,20 @@ trait GameBuilder:
   def build: Game
 
 object GameBuilder:
-  export DSL.*
 
-  private val _gameBuilder: GameBuilder = GameBuilderImpl()
+  def apply(): GameBuilder = GameBuilderImpl()
 
   private class GameBuilderImpl extends GameBuilder:
     private var _gameName: String = ""
     private var _players: Set[String] = Set.empty
+    private var _cardsInHand: Int = 0
+    private var _availableCards = StandardDeck.cards
 
     def reset(): Unit = 
       _gameName = ""
       _players = _players.empty
+      _cardsInHand = 0
+      _availableCards = StandardDeck.cards
 
     def setName(name: String): this.type =
       stringRequirements(name, "Game name")
@@ -58,25 +70,28 @@ object GameBuilder:
         case false => _players = _players + name
       this
 
+    def cardsInHand(numberOfCards: Int): GameBuilder = 
+      require(numberOfCards > 0, "Number of cards in hand must be greater than 0")
+      require(this._cardsInHand == 0, "Number of cards in hand is already set")
+      this._cardsInHand = numberOfCards
+      this
+
     def build: Game = 
+      //create game
       val game = SimpleGame(this._gameName)
+      _players.foreach { name =>
+        // create player
+        val player = SimplePlayer(name)
+        game.addPlayer(player)
+        for _ <- 1 to _cardsInHand do
+          // populate player's deck
+          val card = _availableCards.head
+          player.deck.addCard(card)
+          _availableCards = _availableCards.tail
+      }
       reset()
       game
 
     private def stringRequirements(s: String, name: String) =
       require(s.nonEmpty, s"$name cannot be empty")
       require(s.trim.nonEmpty, s"$name cannot be blank")
-
-  object PlayerCardsBuilder:
-    def method: Unit = ()
-
-  object DSL:
-
-    def Game: GameBuilder = _gameBuilder
-
-    extension (game: GameBuilder)
-      infix def is(name: String): GameBuilder =
-        game.setName(name)
-
-      infix def playersHave(numberOfCards: Int = -1): PlayerCardsBuilder =
-        ???
---
+++ app/src/test/scala/org/cge/engine/GameBuilderTest.scala
@@ -0,0 +1,7 @@
+package org.cge.engine
+
+import org.cge.AnyTest
+
+class GameBuilderTest extends AnyTest:
+  test("first test"):
+    fail()
