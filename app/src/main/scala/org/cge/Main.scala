38eae49d36b9dc13299569e6ff4bcd7a865a28cf
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-10-05 17:50:20 +0200

    refactor: removed unused import in main and fixed a typo in test

--- app/src/main/scala/org/cge/Main.scala
+++ app/src/main/scala/org/cge/Main.scala
@@ -1,17 +1,9 @@
 package org.cge
 
-import engine.Game._
 import engine.view.WindowStateImpl
 import engine.view.Monads.Monad.seqN
 
 object Main:
-  def main(args: Array[String]): Unit =
-    val card1 = SimpleCard("1", "spades")
-    val card2 = SimpleCard("2", "spades")
-    val card3 = SimpleCard("3", "spades")
-
-  def greeting(message: String = "Hello, world!"): String = message
-
   // Testing functional GUI with players and cards
   val windowCreation = for 
     // Set the window size
