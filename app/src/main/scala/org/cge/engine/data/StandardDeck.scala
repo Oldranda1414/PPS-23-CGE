ee0796393802f20698de6aafbfb0a4028cea61ac
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-10-05 17:56:26 +0200

    Fix: fixed formatting in stddeck

--- app/src/main/scala/org/cge/engine/data/StandardDeck.scala
+++ app/src/main/scala/org/cge/engine/data/StandardDeck.scala
@@ -2,14 +2,12 @@ package org.cge.engine.data
 
 import org.cge.engine.Game.SimpleCard
 
-/**
-  * A standard deck of french cards.
-  */
+/** A standard deck of french cards. */
 object StandardDeck:
-    val suits: List[String] = List("Hearts", "Diamonds", "Clubs", "Spades")
-    val values: List[String] = List("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
-    val cards: List[SimpleCard] = for
-        suit <- suits
-        value <- values
-    yield SimpleCard(value, suit)
-  
+  val suits: List[String] = List("Hearts", "Diamonds", "Clubs", "Spades")
+  val values: List[String] =
+    List("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+  val cards: List[SimpleCard] = for
+    suit <- suits
+    value <- values
+  yield SimpleCard(value, suit)
