2bcc19c490d61a0974fe380173d3ecb4de9cbd3a
Oldranda1414 <leonardo.randacio@gmail.com>
2024-10-07 10:48:17 +0200

    refactoring: added test for standard deck

---
+++ app/src/test/scala/org/cge/engine/data/StandardDeckTest.scala
@@ -0,0 +1,24 @@
+package org.cge.engine.data
+
+import org.cge.AnyTest
+import org.cge.engine.model.GameModel.SimpleCard
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.BeforeAndAfterEach
+
+class StandardDeckTest extends AnyTest with BeforeAndAfterEach:
+  
+  test("StandardDeck should have four suits"):
+    StandardDeck.suits should be (List("Hearts", "Diamonds", "Clubs", "Spades"))
+
+  test("StandardDeck should have thirteen values"):
+    StandardDeck.values should be (List("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"))
+
+  test("StandardDeck should have 52 cards"):
+    StandardDeck.cards.size should be (52)
+
+  test("StandardDeck should contain all combinations of suits and values"):
+    val expectedCards = for
+      suit <- StandardDeck.suits
+      value <- StandardDeck.values
+    yield SimpleCard(value, suit)
+    StandardDeck.cards should be (expectedCards)
