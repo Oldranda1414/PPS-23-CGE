178331b08ca71cb6fa941e7cda64cd85f769f42d
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-10-17 09:17:17 +0200

    Refactor: moved out from DSL syntactic builders

--- app/src/main/scala/org/cge/dsl/CardGameEngineDSL.scala
+++ app/src/main/scala/org/cge/dsl/CardGameEngineDSL.scala
@@ -2,6 +2,8 @@ package org.cge.dsl
 
 import org.cge.engine.GameBuilder
 import org.cge.dsl.SyntacticSugar._
+import org.cge.dsl.SyntacticBuilder.PlayerBuilder
+import org.cge.dsl.SyntacticBuilder.CountCardBuilder
 
 object CardGameEngineDSL:
 
@@ -16,55 +18,6 @@ object CardGameEngineDSL:
 
   /** The starting word for the DSL. It defines the start of a new game */
   implicit def game: GameBuilder = builder
-
-  /** This class is used to build a player in the DSL. */
-  trait PlayerBuilder:
-    /**
-     * This method is used to set the name of the player.
-     *
-     * @param name The name of the player
-     * @return The builder
-     */
-    infix def called(name: String): GameBuilder
-
-  private class PlayerBuilderImpl(val builder: GameBuilder)
-      extends PlayerBuilder:
-    infix def called(name: String): GameBuilder = builder.addPlayer(name)
-
-  trait CountCardBuilder:
-    /**
-     * This method is used to set the number of cards in hand for each player.
-     *
-     * @param numberOfCards The number of cards
-     * @return A syntactic sugar builder to complete the sentence
-     */
-    infix def cards(to: ToSyntacticSugar): EachSyntSugarBuilder
-
-  private class CountCardBuilderImpl(
-      val builder: GameBuilder,
-      val numOfCards: Int
-  ) extends CountCardBuilder:
-    infix def cards(to: ToSyntacticSugar): EachSyntSugarBuilder =
-      numOfCards match
-        case -1 =>
-          new EachSyntSugarImpl(
-            builder.cardsInHand(() => scala.util.Random.nextInt(10))
-          )
-        case _ => new EachSyntSugarImpl(builder.cardsInHand(() => numOfCards))
-
-  /** Syntactic sugar builder to complete the sentence 'game gives 5 cards to each player' */
-  trait EachSyntSugarBuilder:
-    /**
-     * This method is used to complete the count card builder sentence.
-     *
-     * @param player syntatcic sugar to enable 'game gives 5 cards to each player' syntax
-     * @return The game builder
-     */
-    infix def each(player: PlayerSyntacticSugar): GameBuilder
-
-  private class EachSyntSugarImpl(val builder: GameBuilder)
-      extends EachSyntSugarBuilder:
-    infix def each(player: PlayerSyntacticSugar): GameBuilder = builder
 
   extension (game: GameBuilder)
     /**
@@ -81,8 +34,7 @@ object CardGameEngineDSL:
      * @param player syntatcic sugar to enable 'game has player called "blabla"' syntax
      * @return The player builder
      */
-    infix def has(syntSugar: PlayerSyntacticSugar): PlayerBuilder =
-      new PlayerBuilderImpl(game)
+    infix def has(syntSugar: PlayerSyntacticSugar): PlayerBuilder = PlayerBuilder(game)
 
     /**
      * This method is used to set the number of cards in hand for each player.
@@ -90,8 +42,7 @@ object CardGameEngineDSL:
      * @param numOfCards The number of cards
      * @return The count card builder
      */
-    infix def gives(numOfCards: Int): CountCardBuilder =
-      new CountCardBuilderImpl(game, numOfCards)
+    infix def gives(numOfCards: Int): CountCardBuilder = CountCardBuilder(game, numOfCards)
 
     /**
      * This method is used to set the number of cards in hand for each player.
@@ -99,8 +50,7 @@ object CardGameEngineDSL:
      * @param random syntactic sugar to enable 'game gives random cards to each player' syntax
      * @return The count card builder
      */
-    infix def gives(random: RandomSyntacticSugar): CountCardBuilder =
-      new CountCardBuilderImpl(game, -1)
+    infix def gives(random: RandomSyntacticSugar): CountCardBuilder = CountCardBuilder(game, -1)
 
     /**
       * This method is used to define what suits will be available in the game.
