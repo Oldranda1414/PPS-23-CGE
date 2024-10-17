e2c6e57e73ce0d695c71affe704fb95fae3d9ca6
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-10-17 22:28:01 +0200

    Feature: implement table with rules

--- app/src/main/scala/org/cge/engine/model/GameModel.scala
+++ app/src/main/scala/org/cge/engine/model/GameModel.scala
@@ -71,7 +71,9 @@ trait TableModel:
   def takeCards(): List[CardModel] 
 
 object TableModel:
-  def apply(): TableModel = SimpleTable()
+  def apply(withRules: Boolean = false): TableModel = withRules match
+    case false => SimpleTable()
+    case true => TableWithRules()
 
   class SimpleTable() extends TableModel:
     private val _cardsOnTable: DeckModel = DeckModel()
@@ -80,3 +82,13 @@ object TableModel:
     def playCard(card: CardModel) = _cardsOnTable.addCard(card)
     def takeCards(): List[CardModel] =
       _cardsOnTable.removeCards(_cardsOnTable.cards.size)
+
+  type PlayingRule = (List[CardModel], CardModel) => Boolean
+  class TableWithRules() extends SimpleTable:
+    private var _rules: List[PlayingRule] = List[PlayingRule]()
+
+    def addPlayingRule(rule: PlayingRule): Unit = _rules = _rules :+ rule
+    def rules: List[PlayingRule] = _rules
+    override def playCard(card: CardModel) = _rules.map[Boolean](r => r(super.cardsOnTable, card)).forall(identity) match
+      case true => super.playCard(card)
+      case false => _rules = _rules
--- app/src/test/scala/org/cge/engine/model/TableModelTest.scala
+++ app/src/test/scala/org/cge/engine/model/TableModelTest.scala
@@ -3,6 +3,8 @@ package org.cge.engine.model
 import org.cge.AnyTest
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.BeforeAndAfterEach
+import org.cge.engine.model.TableModel.TableWithRules
+import org.cge.engine.model.TableModel.PlayingRule
 
 class TableModelTest extends AnyTest with BeforeAndAfterEach:
   var table: TableModel = TableModel()
@@ -38,3 +40,40 @@ class TableModelTest extends AnyTest with BeforeAndAfterEach:
   test("SimpleTable takeCards should return an empty list if no card has been played"):
     val takenCards: List[CardModel] = table.takeCards()
     takenCards should be (List[CardModel]())
+
+class TableWithRulesTest extends AnyTest with BeforeAndAfterEach:
+  var table: TableModel = TableModel(true)
+  // This rule is respected if on the table there are only cards of spades
+  val rule: PlayingRule = (cardsOnTable: List[CardModel], card: CardModel) =>
+    cardsOnTable.filterNot(card => card.suit == Spades).size == 0
+      && card.suit == Spades
+
+  override def beforeEach(): Unit =
+    table = TableModel(true)
+
+  test("With TableWithRules is possible to add playing rules"):
+    val rule: PlayingRule = (_: List[CardModel], _: CardModel) => true
+
+    table match
+      case t: TableWithRules =>
+        t.addPlayingRule(rule)
+        t.rules should be (List(rule))
+      case _ => fail("TableModel was not a TableWithRules, check apply() function.")
+
+  test("TableWithRules sholud accept playing cards following the added rules"):
+    table match
+      case t: TableWithRules =>
+        t.addPlayingRule(rule)
+        t.playCard(CardModel(Ace, Spades))
+        t.playCard(CardModel(Two, Spades))
+        t.cardsOnTable should be (List(CardModel(Ace, Spades), CardModel(Two, Spades)))
+      case _ => fail("TableModel was not a TableWithRules, check apply() function.")
+
+  test("TableWithRules sholud reject playing cards not following the added rules"):
+    table match
+      case t: TableWithRules =>
+        t.addPlayingRule(rule)
+        t.playCard(CardModel(Ace, Spades))
+        t.playCard(CardModel(Two, Clubs))
+        t.cardsOnTable should be (List(CardModel(Ace, Spades)))
+      case _ => fail("TableModel was not a TableWithRules, check apply() function.")
