8799b4389749159cf72a4eea37e39f4abcfac0f0
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-10-23 16:00:20 +0200

    Refactor: refactor PlayerModelTest

--- app/src/test/scala/org/cge/engine/model/PlayerModelTest.scala
+++ app/src/test/scala/org/cge/engine/model/PlayerModelTest.scala
@@ -11,17 +11,17 @@ class PlayerModelTest extends AnyTest with BeforeAndAfterEach:
   override def beforeEach(): Unit =
     player = PlayerModel("name")
 
-  test("SimplePlayer name should be \"name\""):
+  test("PlayerModel name should be \"name\""):
     player.name should be ("name")
 
-  test("SimplePlayer cards set should be empty after initialization"):
+  test("PlayerModel cards set should be empty after initialization"):
     player.hand.cards should be (List[CardModel]())
   
-  test("SimplePlayer cards set should be non-empty after addCard"):
+  test("PlayerModel cards set should be non-empty after addCard"):
     player.hand.addCard(card)
     player.hand.cards should not be (List[CardModel]())
 
-  test("SimplePlayer cards set should contain the added card after addCard"):
+  test("PlayerModel cards set should contain the added card after addCard"):
     player.hand.addCard(card)
     player.hand.cards should contain (card)
 
