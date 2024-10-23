d0194afa726541b87238d5286577c72d36568070
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-10-23 15:59:12 +0200

    Refactor: refactor DeckModelTest

--- app/src/test/scala/org/cge/engine/model/DeckModelTest.scala
+++ app/src/test/scala/org/cge/engine/model/DeckModelTest.scala
@@ -15,24 +15,24 @@ class DeckModelTest extends AnyTest with BeforeAndAfterEach:
   override def beforeEach(): Unit =
     deck = DeckModel()
 
-  test("SimpleDeck should be non-empty after addCard"):
+  test("DeckModel should be non-empty after addCard"):
     deck.addCard(card)
     deck.cards should not be (List[CardModel]())
 
-  test("SimpleDeck should contain the added card"):
+  test("DeckModel should contain the added card"):
     deck.addCard(card)
     deck.cards should contain (card)
   
-  test("SimpleDeck should contain multiple added cards in order"):
+  test("DeckModel should contain multiple added cards in order"):
     cards.foreach(deck.addCard(_))
     deck.cards should be (cards)
   
-  test("Drawing from a SimpleDeck returns the first N cards of the deck"):
+  test("Drawing from a DeckModel returns the first N cards of the deck"):
     cards.foreach(deck.addCard(_))
     val drawnCards = deck.removeCards(numberOfDrawnCards)
     drawnCards should be (cards.take(numberOfDrawnCards))
 
-  test("Drawing from a SimpleDeck removes the first N cards of the deck"):
+  test("Drawing from a DeckModel removes the first N cards of the deck"):
     cards.foreach(deck.addCard(_))
     val drawnCards = deck.removeCards(numberOfDrawnCards)
     (drawnCards ++ deck.cards) should be (cards)
