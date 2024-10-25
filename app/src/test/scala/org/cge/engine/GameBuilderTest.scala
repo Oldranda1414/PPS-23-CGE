e8e7d86d8ea00748f21b45d2a5e155d01dd0315b
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-10-25 16:57:08 +0200

    Refactor: use scalatest dsl to test for exceptions

--- app/src/test/scala/org/cge/engine/GameBuilderTest.scala
+++ app/src/test/scala/org/cge/engine/GameBuilderTest.scala
@@ -21,39 +21,29 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
     gameBuilder = GameBuilder()
 
   test("build should throw an exception if the game name is not set"):
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
   
   test("build should throw an exception if the number of cards in hand is not set"):
     gameBuilder.setName(gameName)
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
 
   test("build should throw an exception if the number of players is not set"):
     gameBuilder.setName(gameName)
     gameBuilder.cardsInHand(() => 5)
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
 
   test("build should throw an exception if suits are not set"):
     gameBuilder.setName(gameName)
     gameBuilder.addPlayer(playerName)
     gameBuilder.cardsInHand(() => 5)
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
 
   test("build should throw an exception if ranks are not set"):
     gameBuilder.setName(gameName)
     gameBuilder.addPlayer(playerName)
     gameBuilder.cardsInHand(() => 5)
     gameBuilder.addSuit("Clubs")
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
 
   test("build should return a valid game"):
     val game = gameBuilder.setName(gameName)
@@ -68,52 +58,34 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
     game.players.foreach(player => player.hand.cards.size should be (5))
 
   test("cannot set the name with empty or blank strings"):
-    intercept[IllegalArgumentException] {
-      gameBuilder.setName("")
-    }
-    intercept[IllegalArgumentException] {
-      gameBuilder.setName(" ")
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.setName("")
+    a [IllegalArgumentException] should be thrownBy gameBuilder.setName(" ")
 
   test("at start current game cards is an empty list"):
     gameBuilder.currentGameCards.size should be (0)
 
   test("cannot set the name twice"):
     gameBuilder.setName(gameName)
-    intercept[IllegalArgumentException] {
-      gameBuilder.setName(gameName)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.setName(gameName)
 
   test("cannot add a player with an empty or blank string"):
-    intercept[IllegalArgumentException] {
-      gameBuilder.addPlayer("")
-    }
-    intercept[IllegalArgumentException] {
-      gameBuilder.addPlayer(" ")
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addPlayer("")
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addPlayer(" ")
 
   test("cannot add the same player twice"):
     gameBuilder.addPlayer(playerName)
-    intercept[IllegalArgumentException] {
-      gameBuilder.addPlayer(playerName)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addPlayer(playerName)
 
   test("cannot set the number of cards in hand twice"):
     gameBuilder.cardsInHand(() => 5)
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHand(() => 5)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHand(() => 5)
 
   test("cannot set the number of cards in hand under 1"):
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHand(() => 0)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHand(() => 0)
 
   test("cannot add a suit twice"):
     gameBuilder.addSuit(suits(0))
-    intercept[IllegalArgumentException] {
-      gameBuilder.addSuit(suits(0))
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addSuit(suits(0))
 
   test("add suits creates a deck with the specified suits"):
     gameBuilder.addSuit(suits(0)).addSuit(suits(1))
@@ -127,9 +99,7 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
 
   test("cannot add ranks twice"):
     gameBuilder.addSortedRanks(ranks)
-    intercept[IllegalArgumentException] {
-      gameBuilder.addSortedRanks(ranks)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addSortedRanks(ranks)
   
   test("add ranks creates a deck with the specified ranks"):
     gameBuilder.addSortedRanks(ranks)
@@ -152,15 +122,13 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
     game.trump should be (Option(trump))
 
   test("trump cannot be set to a suit that is not in the game"):
-    intercept[IllegalArgumentException] {
-      gameBuilder.addSuit(suits(0))
+    a [IllegalArgumentException] should be thrownBy gameBuilder.addSuit(suits(0))
         .setName(gameName)
         .addPlayer(playerName)
         .cardsInHand(() => 5)
         .addSortedRanks(ranks)
         .setTrump("InvalidTrump")
         .build
-    }
 
   test("exception will throw if sum of cards in hand is greater than the number of cards in the deck"):
     gameBuilder.addSortedRanks(ranks.filter(ranks.indexOf(_) < 4))
@@ -168,9 +136,7 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
       .setName(gameName)
       .addPlayer(playerName)
       .cardsInHand(() => 5)
-    intercept[IllegalArgumentException] {
-      gameBuilder.build
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.build
 
   test("a win condition can be set"):
     val wc1: WinCondition = (game, player) => true || game == player
@@ -201,9 +167,7 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
       .setName(gameName)
       .addPlayer(playerName)
       .cardsInHandPerPlayer(() => 5, playerName)
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHandPerPlayer(() => 5, playerName)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHandPerPlayer(() => 5, playerName)
 
   test("cards in hand per player cannot be called if cards in hand is already set"):
     gameBuilder.addSortedRanks(ranks)
@@ -211,9 +175,7 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
       .setName(gameName)
       .addPlayer(playerName)
       .cardsInHand(() => 5)
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHandPerPlayer(() => 5, playerName)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHandPerPlayer(() => 5, playerName)
 
   test("cards in hand cannot be called if cards in hand per palyer is already set"):
     gameBuilder.addSortedRanks(ranks)
@@ -221,18 +183,14 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
       .setName(gameName)
       .addPlayer(playerName)
       .cardsInHandPerPlayer(() => 5, playerName)
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHand(() => 5)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHand(() => 5)
 
   test("cards in hand cannot set the number of cards in hand for not existing player"):
     gameBuilder.addSortedRanks(ranks)
       .addSuit(suits(0)).addSuit(suits(1))
       .setName(gameName)
       .addPlayer(playerName)
-    intercept[IllegalArgumentException] {
-      gameBuilder.cardsInHandPerPlayer(() => 5, player2Name)
-    }
+    a [IllegalArgumentException] should be thrownBy gameBuilder.cardsInHandPerPlayer(() => 5, player2Name)
 
   test("all players must have a number of cards set"):
     gameBuilder.addSortedRanks(ranks)
@@ -241,6 +199,4 @@ class GameBuilderTest extends AnyTest with BeforeAndAfterEach:
       .addPlayer(playerName)
       .addPlayer(player2Name)
       .cardsInHandPerPlayer(() => 5, playerName)
-    intercept[IllegalStateException] {
-      gameBuilder.build
-    }
+    a [IllegalStateException] should be thrownBy gameBuilder.build
