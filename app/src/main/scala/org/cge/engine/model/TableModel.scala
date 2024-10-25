3ad897d7cf8c998ce6541a559a36f7df382b159d
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-10-25 17:18:14 +0200

    Feature: implement hand rules getter and setter

--- app/src/main/scala/org/cge/engine/model/TableModel.scala
+++ app/src/main/scala/org/cge/engine/model/TableModel.scala
@@ -1,13 +1,16 @@
 package org.cge.engine.model
 
 import org.cge.engine.model.TableModel.PlayingRule
+import org.cge.engine.model.TableModel.HandRule
 
 trait TableModel:
   def cardsOnTable: List[CardModel]
   def playCard(card: CardModel): Unit
   def takeCards(): List[CardModel] 
   def addPlayingRule(rule: PlayingRule): Unit
-  def rules: List[PlayingRule]
+  def addHandRule(rule: HandRule): Unit
+  def playingRules: List[PlayingRule]
+  def handRules: List[HandRule]
   def canPlayCard(card: CardModel): Boolean
 
 object TableModel:
@@ -22,16 +25,19 @@ object TableModel:
       tableDeck.removeCards(tableDeck.cards.size)
 
   type PlayingRule = (List[CardModel], CardModel) => Boolean
+  type HandRule = (List[CardModel], CardModel, Suit) => Boolean
 
   class TableWithRules() extends SimpleTable:
-    var rules: List[PlayingRule] = List[PlayingRule]()
+    var playingRules: List[PlayingRule] = List[PlayingRule]()
+    var handRules: List[HandRule] = List[HandRule]()
 
-    def addPlayingRule(rule: PlayingRule): Unit = rules = rules :+ rule
+    def addPlayingRule(rule: PlayingRule): Unit = playingRules = playingRules :+ rule
+    def addHandRule(rule: HandRule): Unit = handRules = handRules :+ rule
 
     def canPlayCard(card: CardModel): Boolean =
-      rules.map[Boolean](r => r(super.cardsOnTable, card)).forall(identity)
+      playingRules.map[Boolean](r => r(super.cardsOnTable, card)).forall(identity)
 
     override def playCard(card: CardModel) =
       canPlayCard(card) match
         case true => super.playCard(card)
-        case false => rules = rules
+        case false => playingRules = playingRules
--- app/src/test/scala/org/cge/engine/model/TableModelTest.scala
+++ app/src/test/scala/org/cge/engine/model/TableModelTest.scala
@@ -4,6 +4,7 @@ import org.cge.AnyTest
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.BeforeAndAfterEach
 import org.cge.engine.model.TableModel.PlayingRule
+import org.cge.engine.model.TableModel.HandRule
 
 class TableModelTest extends AnyTest with BeforeAndAfterEach:
   var table: TableModel = TableModel()
@@ -12,7 +13,7 @@ class TableModelTest extends AnyTest with BeforeAndAfterEach:
   val card3: CardModel = CardModel("3", "Clubs")
 
   // This rule is respected if on the table there are only cards of spades
-  val rule: PlayingRule = (cardsOnTable: List[CardModel], card: CardModel) =>
+  val playingRule: PlayingRule = (cardsOnTable: List[CardModel], card: CardModel) =>
     cardsOnTable.filterNot(card => card.suit == Suit("Spades")).size == 0
       && card.suit == Suit("Spades")
 
@@ -46,26 +47,32 @@ class TableModelTest extends AnyTest with BeforeAndAfterEach:
     val takenCards: List[CardModel] = table.takeCards()
     takenCards should be (List[CardModel]())
 
-  test("In TableModel is possible to add playing rules"):
-    table.addPlayingRule(rule)
-    table.rules should be (List(rule))
+  test("In TableModel it is possible to add playing rules"):
+    table.addPlayingRule(playingRule)
+    table.playingRules should be (List(playingRule))
 
   test("TableModel should mark a playable card as playable"):
-    table.addPlayingRule(rule)
+    table.addPlayingRule(playingRule)
     table.canPlayCard(card2) should be (true)
 
   test("TableModel should mark a non-playable card as non-playable"):
-    table.addPlayingRule(rule)
+    table.addPlayingRule(playingRule)
     table.canPlayCard(card3) should be (false)
 
   test("TableModel sholud accept playing cards following the added rules"):
-    table.addPlayingRule(rule)
+    table.addPlayingRule(playingRule)
     table.playCard(card1)
     table.playCard(card2)
     table.cardsOnTable should be (List(card1, card2))
 
   test("TableModel sholud reject playing cards not following the added rules"):
-    table.addPlayingRule(rule)
+    table.addPlayingRule(playingRule)
     table.playCard(card1)
     table.playCard(card3)
     table.cardsOnTable should be (List(card1))
+
+  test("In TableModel it is possible to add a hand rule"):
+    val handRule: HandRule = (cardsOnTable, card, trump) =>
+      true || cardsOnTable == card || card == trump
+    table.addHandRule(handRule)
+    table.handRules should be (List(handRule))
