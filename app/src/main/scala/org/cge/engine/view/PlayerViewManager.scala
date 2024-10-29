6b2a6a933aca20633ff3a6d70c29feeecfc332dd
Oldranda1414 <leonardo.randacio@gmail.com>
2024-10-29 16:11:06 +0100

    refactor: minor improvements to code readability

--- app/src/main/scala/org/cge/engine/view/PlayerViewManager.scala
+++ app/src/main/scala/org/cge/engine/view/PlayerViewManager.scala
@@ -13,11 +13,8 @@ case object Right extends PlayerPosition
 object PlayerViewManager:
   import org.cge.engine.WindowDimentions.*
 
-  private val horizontalPlayerDims = (windowWidth/2, windowHeight/10)
-  private val verticalPlayerDims = (windowWidth/10, windowHeight/2)
-
-  private val widthPadding = windowWidth/12
-  private val heightPadding = windowHeight/12
+  private val horizontalPlayerDims = (windowWidth / 2, windowHeight / 10)
+  private val verticalPlayerDims = (windowWidth / 10, windowHeight / 2)
 
   private val centerPlayerDims = horizontalPlayerDims
   private val upPlayerDims = horizontalPlayerDims
@@ -25,7 +22,10 @@ object PlayerViewManager:
   private val downPlayerDims = horizontalPlayerDims
   private val leftPlayerDims = verticalPlayerDims
 
-  private val centerPlayerCoords = (windowWidth/5, windowHeight/2)
+  private val widthPadding = windowWidth / 12
+  private val heightPadding = windowHeight / 12
+
+  private val centerPlayerCoords = (windowWidth / 5, windowHeight / 2)
   private val upPlayerCoords = (widthPadding, heightPadding)
   private val rightPlayerCoords = (windowWidth - (rightPlayerDims._1 + widthPadding), heightPadding)
   private val downPlayerCoords = (windowWidth - (downPlayerDims._1 + widthPadding), windowHeight - (downPlayerDims._2 + heightPadding))
@@ -36,13 +36,13 @@ object PlayerViewManager:
 
   def addPlayer(windowState: State[Window, Unit], playerName: String): State[Window, Unit] =
     if (playerPositions.size >= possiblePositions.length)
-      throw new IllegalArgumentException("Cannot add player " + playerName + ": A maximum of 4 players is allowed")
-    else
-      playerPositions += (playerName -> possiblePositions(playerPositions.size))
+      throw new IllegalArgumentException(s"Cannot add player $playerName: A maximum of 4 players is allowed")
     
-    val (playerX, playerY): (Int, Int) = getPlayerHandCoords(playerName)
+    playerPositions += (playerName -> possiblePositions(playerPositions.size))
+
+    val (playerX, playerY) = getPlayerHandCoords(playerName)
     val (playerWidth, playerHeight) = getPlayerDims(playerName)
-    val playerOrientation: Boolean = getPlayerOrientation(playerName)
+    val playerOrientation = getPlayerOrientation(playerName)
 
     for
       _ <- windowState
@@ -50,8 +50,13 @@ object PlayerViewManager:
       _ <- WindowState.addGridLayout(playerName, playerOrientation)
       _ <- WindowState.addPanelTitle(playerName, playerName)
     yield ()
-  
-  private def getPlayerHandCoords(playerName: String): (Int, Int) = 
+    
+  private def getPlayerPosition(playerName: String): PlayerPosition = 
+    playerPositions.get(playerName) match
+      case Some(position) => position
+      case None => throw new NoSuchElementException(s"Player $playerName position not found")
+
+  private def getPlayerHandCoords(playerName: String): (Int, Int) =
     getPlayerPosition(playerName) match
       case Center => centerPlayerCoords
       case Up => upPlayerCoords
@@ -59,25 +64,15 @@ object PlayerViewManager:
       case Left => leftPlayerCoords
       case Right => rightPlayerCoords
 
-  private def getPlayerOrientation(playerName: String): Boolean = 
+  private def getPlayerOrientation(playerName: String): Boolean =
     getPlayerPosition(playerName) match
-      case Center => false
-      case Up => false
-      case Down => false
-      case Left => true
-      case Right => true
+      case Left | Right => true
+      case Center | Up | Down => false
 
-  private def getPlayerDims(playerName: String): (Int, Int) = 
+  private def getPlayerDims(playerName: String): (Int, Int) =
     getPlayerPosition(playerName) match
       case Center => centerPlayerDims
       case Up => upPlayerDims
       case Down => downPlayerDims
       case Left => leftPlayerDims
       case Right => rightPlayerDims
-  
-  private def getPlayerPosition(playerName: String): PlayerPosition =
-    playerPositions.get(playerName) match
-      case Some(position) => position
-      case None => throw new NoSuchElementException(s"Player $playerName position not found")
-    
-  
