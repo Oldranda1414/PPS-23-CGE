09b7b34a6c1cdc7f90e965403429802558abc1ad
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2024-10-30 14:35:21 +0100

    Feature: added points to player model

--- app/src/main/scala/org/cge/engine/model/PlayerModel.scala
+++ app/src/main/scala/org/cge/engine/model/PlayerModel.scala
@@ -3,9 +3,18 @@ package org.cge.engine.model
 trait PlayerModel:
   def name: String
   def hand: DeckModel
+  def points: Int
+  def points_=(value: Int): Unit
 
 object PlayerModel:
   def apply(name: String): PlayerModel = SimplePlayer(name)
 
   final case class SimplePlayer(val name: String) extends PlayerModel:
     val hand: DeckModel = DeckModel()
+
+    private var playerPoints: Int = 0
+
+    def points = playerPoints
+    def points_=(value: Int): Unit = 
+      require(value > 0, "Points must be greater than 0")
+      playerPoints = value
--- app/src/test/scala/org/cge/engine/model/PlayerModelTest.scala
+++ app/src/test/scala/org/cge/engine/model/PlayerModelTest.scala
@@ -25,3 +25,14 @@ class PlayerModelTest extends AnyTest with BeforeAndAfterEach:
     player.hand.addCard(card)
     player.hand.cards should contain (card)
 
+  test("PlayerModel points should be 0 after initialization"):
+    player.points should be (0)
+
+  test("PlayerModel points should be 10 after setting points to 10"):
+    player.points = 10
+    player.points should be (10)
+
+  test("PlayerModel points should throw IllegalArgumentException if set to -1"):
+    an [IllegalArgumentException] should be thrownBy:
+      player.points = -1
+
